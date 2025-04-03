f8ed933563a06f0b43ecf6253197893af915d1ea
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-04-03 16:50:30 -0400

    XD

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
